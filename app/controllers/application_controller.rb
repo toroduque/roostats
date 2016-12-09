cb98d865b12d4a82d78d52de7ab4888defc7dde0
Daniel T <toro_duque@hotmail.com>
2016-12-09 23:30:01 +1100

    more styling a redirects after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
   respond_to :html, :json
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :store_current_location, :unless => :devise_controller?
   protect_from_forgery with: :exception
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, contract_attributes: [:contract_type, :vehicle]])
+  end
+
+  private
+  def store_current_location
+    store_location_for(:rider, riders_path)
   end
 end
